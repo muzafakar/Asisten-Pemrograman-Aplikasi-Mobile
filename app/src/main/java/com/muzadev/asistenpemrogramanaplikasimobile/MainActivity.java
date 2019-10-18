064fde7d65da8512263c81896c0874b30bcc92e9
muzafakar <muhammad.zulfakar@students.amikom.ac.id>
2019-10-18 13:02:05 +0700

    local settings

--- app/src/main/java/com/muzadev/asistenpemrogramanaplikasimobile/MainActivity.java
+++ app/src/main/java/com/muzadev/asistenpemrogramanaplikasimobile/MainActivity.java
@@ -1,8 +1,12 @@
 package com.muzadev.asistenpemrogramanaplikasimobile;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnChangeLanguage = findViewById(R.id.btnChangeLanguage);
+
+        btnChangeLanguage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent implicitIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+                startActivity(implicitIntent);
+            }
+        });
     }
 }
