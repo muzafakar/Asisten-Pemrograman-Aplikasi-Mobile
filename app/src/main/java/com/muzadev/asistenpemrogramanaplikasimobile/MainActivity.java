40a6b7158c2bf2daa3e96c66bf60675bf7771025
muzafakar <muhammad.zulfakar@students.amikom.ac.id>
2019-10-24 19:54:30 +0700

    fragment transaction

--- app/src/main/java/com/muzadev/asistenpemrogramanaplikasimobile/MainActivity.java
+++ app/src/main/java/com/muzadev/asistenpemrogramanaplikasimobile/MainActivity.java
@@ -1,14 +1,62 @@
 package com.muzadev.asistenpemrogramanaplikasimobile;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.muzadev.asistenpemrogramanaplikasimobile.fragments.FavoriteFragment;
+import com.muzadev.asistenpemrogramanaplikasimobile.fragments.HomeFragment;
+import com.muzadev.asistenpemrogramanaplikasimobile.fragments.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private Fragment homeFragment;
+    private Fragment favoriteFragment;
+    private Fragment profileFragment;
+
+    private BottomNavigationView bnvHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bnvHome = findViewById(R.id.bnvHome);
+
+        // inisialisasi object fragment
+        homeFragment = new HomeFragment();
+        favoriteFragment = new FavoriteFragment();
+        profileFragment = new ProfileFragment();
+
+        // menampilkan homeFragment sebagai tampilan awal
+        replaceFragment(homeFragment);
+
+        // memberikan aksi ketika  menu diklik
+        bnvHome.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menuHome:
+                        replaceFragment(homeFragment);
+                        break;
+                    case R.id.menuProfile:
+                        replaceFragment(profileFragment);
+                        break;
+                    case R.id.menuFavorite:
+                        replaceFragment(favoriteFragment);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.mainFragment, fragment)
+                .commit();
     }
 }
